3104e204a772f4358d8531530c8bc567c1afe00f
Kevin <kevinvelasco0299@gmail.com>
2022-06-28 17:42:37 -0500

    Se agrega opcion para abrir enlaces

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http'
+import { SocialSharing } from '@awesome-cordova-plugins/social-sharing/ngx';
+import { InAppBrowser } from '@awesome-cordova-plugins/in-app-browser/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,HttpClientModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },SocialSharing,InAppBrowser],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {NewsService } from '../servicios/news.service'
 import {Article} from '../models'
+import { ActionSheetController, Platform } from '@ionic/angular';
+import { SocialSharing } from '@awesome-cordova-plugins/social-sharing/ngx';
+import { InAppBrowser } from '@awesome-cordova-plugins/in-app-browser/ngx';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -8,7 +11,16 @@ import {Article} from '../models'
 })
 export class Tab1Page implements OnInit{
   public articulos: Article[]=[];
-  constructor(private newService: NewsService) {}
+  
+  constructor(
+    
+    private newService: NewsService,  
+    private actionSheet:ActionSheetController,
+    private socialSharing: SocialSharing, 
+    private platform: Platform,
+    private inApp: InAppBrowser
+    
+    )   { }
   ngOnInit(): void {
     this.getNoticiasDestacadas();
   }
@@ -16,6 +28,43 @@ export class Tab1Page implements OnInit{
   getNoticiasDestacadas(){
     this.newService.getNoticiasDestacadas().subscribe(articulos => 
       this.articulos.push(...articulos) );
+  }
+
+  async opciones(articulo){
+    const actionSheet = await this.actionSheet.create({
+      header: 'Opciones',
+      buttons: [{
+        text: 'Compartir',
+        icon: 'share-outline',
+        handler: () => this.compartir(articulo)
+      },{
+        text: 'Cancelar',
+        icon: 'close-outline',
+        role: 'cancel'
+      }]
+    })
+    await actionSheet.present();
+  }
+
+  compartir(articulo){
+    
+    const {title,source,url} = articulo;
+    this.socialSharing.share(
+      title,
+      source.name,
+      null,
+      url
+    );
+  }
+
+  abrirEnlace(articulo){
+    if(this.platform.is('ios') || this.platform.is('android')){
+      const browser = this.inApp.create(articulo.url);
+      browser.show();
+      return
+    }  
+    
+    window.open(articulo.url, '_blank');
   }
 
 }
